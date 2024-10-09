791e3f4be26597f7a2b0a127bcad3920f5f41ce3
Alex Bascevan <alexbascevan@icloud.com>
2024-10-09 08:14:49 -0400

    Initial Commit

---
+++ StickyNotes/StickyNotesApp.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNotesApp.swift
+//  StickyNotes
+//
+//  Created by Alexander Bascevan on 2024-10-09.
+//
+
+import SwiftUI
+
+@main
+struct StickyNotesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ StickyNotesTests/StickyNotesTests.swift
@@ -0,0 +1,17 @@
+//
+//  StickyNotesTests.swift
+//  StickyNotesTests
+//
+//  Created by Alexander Bascevan on 2024-10-09.
+//
+
+import Testing
+@testable import StickyNotes
+
+struct StickyNotesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
