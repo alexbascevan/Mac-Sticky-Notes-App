522e225e3651d23c61e8c101e046b4fc1b347ec0
Alex Bascevan <alexbascevan@icloud.com>
2024-10-09 12:46:10 -0400

    Added in homescreen Widget

---
+++ Note/Note.swift
@@ -0,0 +1,34 @@
+//  Note.swift
+//  StickyNotes
+//
+//  Created by Alexander Bascevan on 2024-10-09.
+//
+
+import Foundation
+
+// Model class representing a single note
+class Note: Identifiable, ObservableObject, Hashable, Codable {
+    // Unique identifier for the note
+    let id: UUID
+    
+    // Properties to allow UI to react to changes
+    var title: String
+    var content: String
+    
+    // Initializer to set up a new note with optional title and mandatory content
+    init(title: String = "", content: String) {
+        self.id = UUID() // Generate a unique ID
+        self.title = title // Set the note's title
+        self.content = content // Set the note's content
+    }
+    
+    // Hashable protocol requirement: checks equality based on ID
+    static func == (lhs: Note, rhs: Note) -> Bool {
+        return lhs.id == rhs.id // Two notes are equal if their IDs match
+    }
+    
+    // Hashable protocol requirement: provides a hash value for the note
+    func hash(into hasher: inout Hasher) {
+        hasher.combine(id) // Combine the ID into the hasher
+    }
+}
---
+++ StickyNotesWidget/StickyNotesWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  StickyNotesWidgetBundle.swift
+//  StickyNotesWidget
+//
+//  Created by Alexander Bascevan on 2024-10-09.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct StickyNotesWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        StickyNotesWidget()
+    }
+}
